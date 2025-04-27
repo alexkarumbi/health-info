42fceccda6520398e03498616e8fe93953a955b1
alexkarumbi <alexkarumbimuhia@gmail.com>
2025-04-27 14:41:57 +0300

    Added icons component

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import * as React from 'react'
+import { LucideProps } from 'lucide-react'
+import {
+  Home,
+  Users,
+  Stethoscope,
+  Calendar,
+  FileText,
+  Settings,
+  LogOut,
+  Plus,
+  Search,
+  ChevronDown,
+  MoreVertical,
+  Check,
+  X,
+} from 'lucide-react'
+
+export const Icons = {
+  dashboard: Home,
+  users: Users,
+  medical: Stethoscope,
+  calendar: Calendar,
+  reports: FileText,
+  settings: Settings,
+  logout: LogOut,
+  add: Plus,
+  search: Search,
+  chevronDown: ChevronDown,
+  more: MoreVertical,
+  check: Check,
+  close: X,
+}
